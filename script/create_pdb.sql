d9605cc9930603d8b479ad045a9dbda36fcce163
Ouahid Yassine <ouahid.1982001@gmail.com>
2025-12-23 03:08:42 +0100

    Day 1: PDB provisioning

---
+++ script/create_pdb.sql
@@ -0,0 +1,4 @@
+CREATE PLUGGABLE DATABASE PDB_BASKETBALLP_19C
+ADMIN USER B_admin IDENTIFIED BY AdminPass123
+FILE_NAME_CONVERT = ('/opt/oracle/oradata/ORCLCDB/pdbseed/', 
+                     '/opt/oracle/oradata/ORCLCDB/PDB_BASKETBALLP_19C/');
